efd1ea817c959526ffd5906fc5556dc5f078058d
Luis Gustavo <67930760+ilunnie@users.noreply.github.com>
2021-11-28 23:41:57 -0300

    Delete Unlimited directory

--- Unlimited/js/home.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelectorAll('.col-md-3 img');
-
-btn.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-})
-
-function scrollToIdOnClick(event) {
-    smoothScrollTo(0, 0);
-  }
